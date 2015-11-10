cae379c1496d1e05e1046e4d2f5f12a157411575
Brett Chien <brebre_no7@yahoo.com>
2015-11-09 22:36:12 -0800

    checkpoint26

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@
  var createSongRow = function(songNumber, songName, songLength, songPlays) {
       var template =
          '<tr class="album-view-song-item">'
-       + '  <td class="song-item-number">' + songNumber + '</td>'
+       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
        + '  <td class="song-item-title">' + songName + '</td>'
        + '  <td class="song-item-duration">' + songLength + '</td>'
        + '  <td class="song-item-plays">' + songPlays + '</td>'
@@ -81,9 +81,34 @@
           albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length, album.songs[i].plays);
       }
   };
-  
+
+  // Elements we'll be adding listeners to
+  var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+  var songRows = document.getElementsByClassName('album-view-song-item');
+
+
+
   window.onload = function() {
       setCurrentAlbum(albumPicasso);
+
+      songListContainer.addEventListener('mouseover', function(event) {
+          // Only target individual song rows during event delegation
+          if (event.target.parentElement.className === 'album-view-song-item') {
+              // Change the content from the number to the play button's HTML
+              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+
+          }
+      });
+
+      for (i = 0; i < songRows.length; i++) {
+          songRows[i].addEventListener('mouseleave', function(event) {
+          // Selects first child element, which is the song-item-number element 
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+          });
+      }
+
+      // Album button templates
+       var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
       // get the album cover and save it to a variable
       // Add the click event listener to the album
