db928216f2ec091debbfde0e327cfaa9d37c368c
Brett Chien <brebre_no7@yahoo.com>
2015-11-16 22:30:18 -0800

    checkpoint33 - 1

--- scripts/album.js
+++ scripts/album.js
@@ -24,15 +24,21 @@
             // Switch from Play -> Pause button to indicate new song is playing.
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();
 
            } else if (currentlyPlayingSongNumber === songNumber) {
-            // Switch from Pause -> Play button to pause currently playing song.
-            $(this).html(playButtonTemplate);
-
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-            currentlyPlayingSongNumber = null;
-            currentSongFromAlbum = null;
+                //we need to start playing the song again and revert the icon in the song row and the player bar to the pause button.
+                if (currentSoundFile.isPaused()) {
+                  $(this).html(pauseButtonTemplate);
+                  $('.main-controls .play-pause').html(playerBarPauseButton);
+                  currentSoundFile.play();
+              } else {
+                //we need to pause it and set the content of the song number cell and player bar's pause button back to the play button.
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+              }
            }
        };
        
@@ -120,11 +126,11 @@
       currentlyPlayingSongNumber = currentSongIndex + 1;
       currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
+      //
+      currentSoundFile.play();
+
       // Update the Player Bar information
-      $('.currently-playing .song-name').text(currentSongFromAlbum.name);
-      $('.currently-playing .artist-name').text(currentAlbum.artist);
-      $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.name);
-      $('.main-controls .play-pause').html(playerBarPauseButton);
+      updatePlayerBarSong();
       
       var lastSongNumber = getLastSongNumber(currentSongIndex);
       var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
@@ -150,16 +156,16 @@
       if (currentSongIndex < 0) {
           currentSongIndex = currentAlbum.songs.length - 1;
       }
-      
+  
       // Set a new current song
       currentlyPlayingSongNumber = currentSongIndex + 1;
       currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
+      //
+      currentSoundFile.play();
+      
       // Update the Player Bar information
-      $('.currently-playing .song-name').text(currentSongFromAlbum.name);
-      $('.currently-playing .artist-name').text(currentAlbum.artist);
-      $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.name);
-      $('.main-controls .play-pause').html(playerBarPauseButton);
+      updatePlayerBarSong();
       
       var lastSongNumber = getLastSongNumber(currentSongIndex);
       var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
@@ -171,10 +177,29 @@
   };
 
   var setSong = function (songNumber){
+      if (currentSoundFile) {
+          currentSoundFile.stop();
+      }
+      
       //assign currentlyPlayingSongNumber and currentSongFromAlbum a new value based on the new song number.
       currentlyPlayingSongNumber =parseInt(songNumber) ;
       currentSongFromAlbum = currentAlbum.songs[songNumber-1]; // -1 value because first song's index is 0
-  };
+      // #1
+      currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+          // #2
+          formats: [ 'mp3' ],
+          preload: true
+      });
+
+          setVolume(currentVolume);
+    };
+      
+  var setVolume = function(volume) {
+      if (currentSoundFile) {
+          currentSoundFile.setVolume(volume);
+      }
+    };
+
 
   var getSongNumberCell = function (number) {
       return $('.song-item-number[data-song-number="' + number + '"]');
@@ -199,6 +224,10 @@
    var currentAlbum = null;
    var currentlyPlayingSongNumber = null;
    var currentSongFromAlbum = null;
+   var currentSoundFile = null;
+   var currentVolume = 80;
+
+
 
    var $previousButton = $('.main-controls .previous');
    var $nextButton = $('.main-controls .next');
